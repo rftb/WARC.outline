6537180e75a6af27430df7f1b5491377bdf895a6
RFTB <preston@rideforthebrand.net>
2014-07-11 15:15:57 -0500

    a

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ jQuery( document ).ready(function($) {
 
   $('.scroll').height(viewport.h-40);
 
-  $('ol.timeline').find('a').on('click', function(e){
+  $('.sidebar').find('a').on('click', function(e){
     e.preventDefault();
-    $('ol.timeline').find('a').removeClass('active');
+    $('.sidebar').find('a').removeClass('active');
     $(this).addClass('active');
     $( "#details" ).load( $(this).attr('href'), function() {
       console.log('ok');
